15a3e22aab7c104bbe1be68fa8ed41cf210a75fc
Prashant Vardhan <prashant.vardhan@inveera.us>
2015-07-03 15:11:50 +0530

    ---

--- app/jobs/subscribe_user_to_mailing_list_job.rb
+++ app/jobs/subscribe_user_to_mailing_list_job.rb
@@ -2,6 +2,6 @@ class SubscribeUserToMailingListJob < ActiveJob::Base
 
   def perform(user)
     gb = Gibbon::API.new
-    gb.lists.subscribe({:id => ENV["MAILCHIMP_LIST_ID"], :email => {:email => user.email}})
+    gb.lists.subscribe({:id => Rails.application.secrets.MAILCHIMP_LIST_ID, :email => {:email => user.email}})
   end
 end
