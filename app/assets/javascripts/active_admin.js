164ecfd21683837b33dbaf4aac151fe702d898d9
Naman <naman.srivastav@inveera.us>
2015-07-08 12:19:45 +0530

    --

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,7 +1,107 @@
 //= require active_admin/base
+$(function (){
+    $('#student_dob').datepicker({
+        changeYear: true,
+        changeMonth: true,
+        yearRange: "1950:2050"
+    });
+    $('#student_enroll_date').datepicker({
+        changeYear: true,
+        changeMonth: true,
+        yearRange: "1950:2050"
+    });
+    $('#student_graduation_date').datepicker({
+        changeYear: true,
+        changeMonth: true,
+        yearRange: "1950:2050"
+    });
+
+});
 $(document).ready(function(){
+	var wrapper         = $(".input_fields_wrap"); //Fields wrapper
+    var add_button      = $(".add_field_button"); //Add button ID
+    var x = 1;    
+    var xval = $("#xval").val();
+    $(add_button).click(function(e){ //on add input button click
+        e.preventDefault();
+        // debugger
+        if (typeof xval !== 'undefined' )
+        {
+            x=$("#xval").val();
+        }
+        x++; 
+        $(wrapper).append('<div>'+
+            '<h3>Educational Detail '+x+'</h3> <a href="#" class="remove_field">Remove</a>'+
+            '<ol><li class="string input optional stringish">'+
+            '<label for="grad_name">Graduation Name</label>'+
+            '<input type="text" name="grad_name[]">'+
+            '</li>'+
+
+            '<li class="string input optional stringish">'+
+            '<label for="completion_year">Completion Year</label>'+
+            '<input type="text" name="completion_year[]">'+
+            '</li>'+
+
+            '<li class="string input optional stringish">'+
+            '<label for="description">Description</label>'+
+            '<input type="text" name="description[]">'+
+            '</li>'+
+
+            '<li class="string input optional stringish">'+
+            '<label for="subject">Subject</label>'+
+            '<input type="text" name="subject[]">'+
+            '</li>'+
+
+            '<li class="string input optional stringish">'+
+            '<label for="course_name">Course Name</label>'+
+            '<input type="text" name="course_name[]">'+
+            '</li>'+
+
+            '<li class="string input optional stringish">'+
+            '<label for="honors">Honors</label>'+
+            '<input type="text" name="honors[]">'+
+            '</li>'+
+
+            '<li class="string input optional stringish">'+
+            '<label for="grade">Grade</label>'+
+            '<input type="text" name="grade[]">'+
+            '</li>'+
+
+            '<li class="string input optional stringish">'+
+            '<label for="credits">Credits</label>'+
+            '<input type="text" name="credits[]">'+
+            '</li>'+
+
+            '<li class="string input optional stringish">'+
+            '<label for="total_credit">Total Credit</label>'+
+            '<input type="text" name="total_credit[]">'+
+            '</li>'+
+
+            '<li class="string input optional stringish">'+
+            '<label for="gpa_credit">Gpa Credit</label>'+
+            '<input type="text" name="gpa_credit[]">'+
+            '</li>'+
+
+            '<li class="string input optional stringish">'+
+            '<label for="gpa_points">Gpa Points</label>'+
+            '<input type="text" name="gpa_points[]">'+
+            '</li>'+
+
+            '<li class="string input optional stringish">'+
+            '<label for="cumulative_gpa">Cumulative Gpa</label>'+
+            '<input type="text" name="cumulative_gpa[]">'+
+            '</li></ol>'+
+            '</div>'); 
+    });
+
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        // e.preventDefault(); 
+        $(this).parent('div').remove();
+        x--;
+    });
 	$('select#user_country').val('US');
-	$('select#school_country').val('US');
+    $('select#school_country').val('US');
+	$('select#student_country').val('US');
 	$('select#user_status').val(1);
 	$('select#user_country').change(function(){
 		select_wrapper = $('#user_state');
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ActiveRecord::Base
-  belongs_to :school
-  has_many :academics
+	belongs_to :school
+  # has_many :academics
+  validates :school_id,  :presence => {:message => "Please select a school" }
+  validates_format_of :email,:with => Devise::email_regexp
 end
+
