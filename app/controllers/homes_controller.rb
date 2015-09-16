a5b2117d74336d5ba0e38673edb02b244982c5eb
sonu kumar <sonu.kumar@inveera.us>
2015-09-16 18:30:36 +0530

    dashboard issue

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,11 +5,15 @@ class HomesController < ApplicationController
 
 
 	def dashboard
-		if !current_user.subscription.blank?
-			subs_plan_id=current_user.subscription.plan_id
-			@subs_plan_name=Plan.find(subs_plan_id).name
+		if !current_user.blank?
+			if !current_user.subscription.blank?
+				subs_plan_id=current_user.subscription.plan_id
+				@subs_plan_name=Plan.find(subs_plan_id).name
+			else
+				@subs_plan_name="Trial"
+			end
 		else
-			@subs_plan_name="Trial"
+			redirect_to root_path
 		end
 	end
 end
