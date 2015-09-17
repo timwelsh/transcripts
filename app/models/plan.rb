b60019986feaa4829740864a20bbb7894ff6666c
sonu kumar <sonu.kumar@inveera.us>
2015-09-17 18:59:15 +0530

    Plan validation

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,4 +1,6 @@
 class Plan < ActiveRecord::Base
 	has_many :subscriptions,dependent: :destroy
-	
+	validates :name,length: { maximum: 50 ,message: "Plan Name should be Maximum of 50 Letter "}
+	validates :amount,numericality:{ only_integer: true ,message:"Amount should be numeric"},length: { maximum: 8 ,message: "Amount should be Maximum of 8 Digit "}
+	validates :subscription_period,numericality:{ only_integer: true ,message:"Subscription Period be numeric"}, inclusion: { :in=> 1..60 ,message: "Subscription Period should be Maximum of 60 months "}
 end
