9a7734d800d25b13cfd7269c336e372c483d3cc5
Prashant Vardhan <prashant.vardhan@inveera.us>
2015-10-15 17:16:04 +0530

    host change for server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '50.116.58.30', port: 6060 }
   config.action_mailer.default :charset => "utf-8"
 
   # Print deprecation notices to the Rails logger.
